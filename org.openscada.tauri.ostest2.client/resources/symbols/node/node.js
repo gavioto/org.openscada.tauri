448fbf78ac25b55f19b6d1c474264dd008c932bf
Jens Reimann <ctron@dentrassi.de>
2013-08-22 14:17:42 +0200

    added some detail stuff

---
+++ org.openscada.tauri.ostest2.client/resources/symbols/node/node.js
@@ -0,0 +1,5 @@
+function makeHeaderProperties() {
+	properties.put("detailsId", "org.openscada.tauri.ostest2.client.details.node");
+	// properties.put("maintenancePrefix", maintenancePrefix());
+	properties.put("sumItem", prefixed("SUM.V"));
+}
